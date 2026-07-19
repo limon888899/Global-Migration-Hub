f397110d478121a60b83536e9c86731d08e78362
Mohammad Limon <solaymanlimon420@gmail.com>
2026-07-19 21:51:14 +0600

    Add BIOMETRIC_CENTERS with location details

---
+++ lib/biometric/centers.ts
@@ -0,0 +1,9 @@
+import type { BiometricCenter } from "./types"
+
+// TODO: আসল সেন্টার ঠিকানা দিয়ে আপডেট করো
+export const BIOMETRIC_CENTERS: BiometricCenter[] = [
+  { id: "dhaka-gulshan", name: "Global Migration Hub – Gulshan Center", city: "Dhaka", address: "Road 11, Gulshan-1, Dhaka 1212" },
+  { id: "dhaka-uttara", name: "Global Migration Hub – Uttara Center", city: "Dhaka", address: "Sector 7, Uttara, Dhaka 1230" },
+  { id: "chattogram", name: "Global Migration Hub – Chattogram Center", city: "Chattogram", address: "GEC Circle, Chattogram 4000" },
+  { id: "sylhet", name: "Global Migration Hub – Sylhet Center", city: "Sylhet", address: "Zindabazar, Sylhet 3100" },
+]
